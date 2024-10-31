77f96c3e164b366bb9c0ecce5b7927ff6859773e
Hadian Rahmat <hadian.rahmat@gmail.com>
2024-10-31 09:36:19 +0700

    initial commit

---
+++ src/data/info.ts
@@ -0,0 +1,131 @@
+export const info = {
+  baseUrl: "https://astro-portfolio-uzair.vercel.app",
+  name: "Hadian Rahmat",
+  jobDescription: "Engineering Manager",
+  about: `I'm a passionate engineering leader with 7 years of experience in the software development field.  Previously, I thrived as a Backend Engineer and Chapter Lead Engineer, building and maintaining robust backend systems.  In my current role as Engineering Manager, I leverage my technical expertise to guide and empower high-performing teams to deliver innovative solutions.
+  Actively engaged in the IT community, I am passionate about knowledge-sharing and networking. Through participation in industry events, conferences, and online platforms, I contribute insights from my career journey and expertise in software engineering, backend development, web technologies, and cloud computing.
+  `,
+
+  experience: [
+    {
+      name: "GITS Indonesia",
+      location: "Engineering Manager",
+      startDate: "Sep 2022",
+      endDate: "Sep 2024",
+      description: [
+        "I lead a team of talented software engineers to develop and maintain high-quality products. With over 5 years of experience in the tech industry, I have a proven track record of delivering successful projects on time and within budget. In my current role, I prioritize collaboration and clear communication to ensure that my team has the support and resources they need to do their best work. I also stay up-to-date on industry best practices and emerging technologies to ensure that we are using the right tools and approaches to meet the evolving needs of our customers.",
+        "- Improving team productivity, efficiency, and collaboration leading to faster project delivery and higher-quality outcomes.",
+        "- Leading projects from conception to completion, meeting or exceeding project goals, timelines, and budgets while ensuring high-quality deliverables.",
+        "- Implementing process improvements and best practices such as Agile methodologies, CI/CD pipelines, and DevOps practices to streamline development workflows and increase efficiency.",
+        ""
+      ],  
+    },
+
+    {
+      name: "",
+      location: "Chapter Lead Backend",
+      startDate: "Mar 2021",
+      endDate: "Sep 2022",
+      description: [
+        "I manage a team of experienced software engineers who develop and maintain the server-side of our products. I am skilled in leading cross-functional teams and implementing best practices to ensure the success of our projects. In my current role, I work closely with the product and design teams to deliver scalable and reliable backend solutions that meet the needs of our customers. I also mentor and provide guidance to my team members to help them grow and succeed in their careers.",
+        "-"
+      ],
+    },
+
+    {
+      name: "",
+      location: "Backend Engineer",
+      startDate: "Jun 2018",
+      endDate: "Mar 2021",
+      description: [
+        "✓ Developing code in accordance with appropriate design and architectural rules",
+        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
+        "✓ Help and training of other colleagues",
+        "✓ Others tasks connected to the nature of the job",
+        "✓ Code Improvements and Refactoring ",
+        "✓ Best Practices and Rules Maintainer",
+      ],
+    },
+
+    {
+      name: "PT Jaya Data Informatika",
+      location: "Web Developer",
+      startDate: "Aug 2017",
+      endDate: "Jun 2018",
+      description: [
+        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
+        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+      ],
+    },
+  ],
+
+  education: [
+    {
+      name: "Widyatama University",
+      location: "Bandung, West Java, Indonesia",
+      startDate: "2018",
+      endDate: "2022",
+      description: [
+        "Information System",
+        "In my studies, I gained a strong foundation in computer science, programming, and systems analysis, with a focus on business applications. I also developed my communication and teamwork skills through group projects and internships."
+      ],
+    },
+    {
+      name: "SMK Negeri 4 Bandung",
+      location: "Bandung, West Java, Indonesia",
+      startDate: "2014",
+      endDate: "2017",
+      description: ["Software Engineering",
+        "During my time at SMK Negeri 4 Bandung, I honed my skills in software development by completing numerous projects, including Bandung SmartBus. I gained proficiency in programming languages such as Java, PHP, and C++, and I have a strong understanding of object-oriented programming principles. Additionally, I am experienced in using version control systems like Git and have contributed to open-source projects on GitHub. My coursework also covered database design, web development, and software testing methodologies."
+      ],
+    }
+  ],
+
+  organization: [
+    {
+      name: "Organizer GDG Bandung",
+      location: "Bandung, West Java, Indonesia",
+      startDate: "2022",
+      endDate: "Present",
+      description: [
+        "GDG Bandung is a technology community dedicated to developing the technology ecosystem in Bandung, West Java, Indonesia. GDG stands for Google Developer Group, which is a global network of developer communities interested in Google technologies."
+      ],
+    },
+    {
+      name: "Organizer BandungDev",
+      location: "Bandung, West Java, Indonesia",
+      startDate: "2023",
+      endDate: "Present",
+      description: [
+        "GDG Bandung is a technology community dedicated to developing the technology ecosystem in Bandung, West Java, Indonesia. GDG stands for Google Developer Group, which is a global network of developer communities interested in Google technologies."
+      ],
+    },
+    {
+      name: "GDG On Campus Lead Widyatama",
+      location: "Bandung, West Java, Indonesia",
+      startDate: "2020",
+      endDate: "2021",
+      description: [
+        "GDG (Google Developer Group) on Campus Widyatama University is a student community focused on technology development, programming, and innovation. Members of GDG on Campus Widyatama University engage in various activities such as training, workshops, seminars, and technology projects aimed at enhancing understanding and skills in software development, artificial intelligence, application development, and other cutting-edge technologies. This community collaborates with Google to provide access to resources, mentors, and opportunities to contribute to relevant technology projects. GDG on Campus Widyatama University encourages collaboration, learning, and creativity among students with interests in the technology field."
+      ],
+    }
+  ],
+
+  socialMedia: {
+    facebook: "https://www.facebook.com/hadian.rahmat.9/",
+    twitter: "https://x.com/hadianr_",
+    github: "https://github.com/hadianr",
+    email: "mailto:hadian.rahmat@gmail.com",
+    linkedin: "https://www.linkedin.com/in/hadianr/",
+  },
+
+  projects: [
+    {
+      title: "Astro Portfolio",
+      isFeatured: true,
+      thumbnail: "/assets/images/astro-portfolio.png",
+      githubUrl: "https://github.com/uzzii-21/astro-portfolio",
+      liveUrl: "https://google.com",
+    },
+  ],
+};
