67ddcee7ed0da5b72dcf071dd14b0539b5becd10
Hadian Rahmat <hadian.rahmat@gmail.com>
2024-10-31 10:15:51 +0700

    docs: revise information about experiences and community

--- src/data/info.ts
+++ src/data/info.ts
@@ -13,11 +13,12 @@ export const info = {
       startDate: "Sep 2022",
       endDate: "Sep 2024",
       description: [
-        "I lead a team of talented software engineers to develop and maintain high-quality products. With over 5 years of experience in the tech industry, I have a proven track record of delivering successful projects on time and within budget. In my current role, I prioritize collaboration and clear communication to ensure that my team has the support and resources they need to do their best work. I also stay up-to-date on industry best practices and emerging technologies to ensure that we are using the right tools and approaches to meet the evolving needs of our customers.",
+        "I lead a team of talented software engineers to develop and maintain high-quality products. With experience in the tech industry, I have a proven track record of delivering successful projects on time and within budget. In my current role, I prioritize collaboration and clear communication to ensure that my team has the support and resources they need to do their best work. I also stay up-to-date on industry best practices and emerging technologies to ensure that we are using the right tools and approaches to meet the evolving needs of our customers.",
         "- Improving team productivity, efficiency, and collaboration leading to faster project delivery and higher-quality outcomes.",
         "- Leading projects from conception to completion, meeting or exceeding project goals, timelines, and budgets while ensuring high-quality deliverables.",
         "- Implementing process improvements and best practices such as Agile methodologies, CI/CD pipelines, and DevOps practices to streamline development workflows and increase efficiency.",
-        ""
+        "- Cross-Functional Collaboration: Facilitating effective collaboration between engineering teams and other departments such as product management, design, and QA to ensure alignment and smooth project execution.",
+        "- Technical Innovation: Spearheading initiatives to explore and adopt new technologies, tools, and frameworks to enhance product features, performance, and scalability."
       ],  
     },
 
@@ -28,7 +29,10 @@ export const info = {
       endDate: "Sep 2022",
       description: [
         "I manage a team of experienced software engineers who develop and maintain the server-side of our products. I am skilled in leading cross-functional teams and implementing best practices to ensure the success of our projects. In my current role, I work closely with the product and design teams to deliver scalable and reliable backend solutions that meet the needs of our customers. I also mentor and provide guidance to my team members to help them grow and succeed in their careers.",
-        "-"
+        "- Team Growth and Development: Successfully mentoring junior engineers, fostering their technical growth, and helping them advance their careers within the organization.",
+        "- Technical Leadership: Providing guidance and leadership to backend engineering teams, ensuring alignment with best practices, architectural standards, and coding conventions.",
+        "- Project Delivery: Leading backend development efforts on complex projects, ensuring timely delivery of high-quality, scalable solutions that meet or exceed client requirements.",
+        "- Code Quality Improvement: Implementing initiatives to improve code quality, readability, and maintainability through code reviews, refactoring, and adherence to coding standards."
       ],
     },
 
@@ -38,12 +42,15 @@ export const info = {
       startDate: "Jun 2018",
       endDate: "Mar 2021",
       description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
+        "I am responsible for developing and maintaining the server-side of our products. In my current role, I work closely with the rest of the engineering team to design and implement scalable, efficient, and reliable backend solutions. I also stay up-to-date on the latest industry best practices and technologies to ensure that we are using the right tools to meet the evolving needs of our customers.",
+        "- Scalable Architecture: Designing and implementing scalable backend architectures that accommodate rapid user growth and ensure high performance under heavy loads. ",
+        "- Optimized Database Performance: Optimizing database queries, indexing strategies, and data models to improve overall system performance and reduce latency.",
+        "- Security Enhancements: Implementing robust security measures such as encryption, authentication, and authorization to protect sensitive data and prevent security breaches.",
+        "- Feature Development: Leading the development of key backend features or modules that enhance product functionality, usability, and customer satisfaction.",
+        "- API Design and Documentation: Designing intuitive and well-documented APIs that facilitate seamless integration with frontend applications and third-party services.",
+        "- Bug Fixing and Maintenance: Proactively identifying and resolving bugs or performance issues in the backend codebase, ensuring a stable and reliable application experience for users.",
+        "- Code Quality and Standards: Maintaining high standards of code quality, readability, and maintainability through code reviews, coding standards, and best practices.",
+        "- Continuous Learning: Actively seeking opportunities for learning and professional development to stay updated on industry trends, emerging technologies, and best practices in backend development."
       ],
     },
 
@@ -53,8 +60,7 @@ export const info = {
       startDate: "Aug 2017",
       endDate: "Jun 2018",
       description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
+        "I am a Full-Stack Developer with a comprehensive skill set spanning both front-end and back-end development. I have experience in designing and developing complete web applications from the ground up. My expertise includes Laravel and Codeigniter. I am passionate about creating innovative and efficient web solutions.",
       ],
     },
   ],
@@ -97,7 +103,7 @@ export const info = {
       startDate: "2023",
       endDate: "Present",
       description: [
-        "GDG Bandung is a technology community dedicated to developing the technology ecosystem in Bandung, West Java, Indonesia. GDG stands for Google Developer Group, which is a global network of developer communities interested in Google technologies."
+        "BandungDev is a tech community for software developers, programmers, engineers in Bandung area and around it. Therefore each event or discussion would be either combining different topics or focusing on just one tech in particular."
       ],
     },
     {
